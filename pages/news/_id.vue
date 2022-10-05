9ba88837da80d6a6bf5de035f2aecb0d2d80f90d
Prys Pegasus <themeprys@gmail.com>
2022-10-05 13:34:14 +0700

    remove limit

--- pages/news/_id.vue
+++ pages/news/_id.vue
@@ -19,6 +19,34 @@ export default {
         };
       },
     },
-  },    
+  },
+head() {
+    return {
+      title: this.getObjects.objects.title,
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: "description",
+          name: "description",
+          content: this.content,
+        },
+        {
+          hid: "og:title",
+          name: "og:title",
+          content: this.getObjects.objects.title,
+        },
+        {
+          hid: "og:site_name",
+          name: "og:site_name",
+          content: this.title,
+        },
+        {
+          hid: "og:description",
+          name: "og:description",
+          content: this.content,
+        },
+      ],
+    };
+  },      
 };
 </script>
